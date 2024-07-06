44aa9f0b3bbfdd9c979742917e0b1443d16e7c92
takemotoooo-eeic <takemoto-kengo2262@g.ecc.u-tokyo.ac.jp>
2024-07-06 11:05:13 +0900

    record_audioを

--- main_audio.py
+++ main_audio.py
@@ -1,18 +1,14 @@
 import threading
 from vad import vad
-from recorder_hsr import record_audio 
+from recorder_hsr import record_audio
 
 def main():
     file_dir = "data/audio"
-
     vad_thread = threading.Thread(target=vad, args=(file_dir,))
-    record_thread = threading.Thread(target=record_audio)
-
     vad_thread.start()
-    record_thread.start()
 
+    record_audio()
     vad_thread.join()
-    record_thread.join()
 
 if __name__ == "__main__":
     main()
